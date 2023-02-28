cfed20620792c1a963b82c94c89de7a8d9f252ec
Divyansh Singh <singh.divyansh1802@gmail.com>
2023-02-28 12:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
-function calculateMinimumCost() {
-	var input = document.querySelector("input").value;
-	var ropeLengths = input.split(",");
-  const n = ropeLengths.length;
+function calculateMinCost() {
 
-  // Sort the rope lengths in non-decreasing order
-  ropeLengths.sort((a, b) => a - b);
+  var inputElement = document.getElementById('rope-lengths').value;
+  var arr = inputElement.split(',');
+   arr.sort(function (a,b) {return a-b});
 
-  // Keep track of the total cost of connecting ropes
-  let totalCost = 0;
+	var cost = 0;
+	while (arr.length>1){
+		var res = Number(arr[0])+ Number(arr[1]);
+        arr.shift()
+        arr.shift()
+		arr.push(res);
+		cost +=res;
 
-  // Keep connecting ropes until there is only one rope left
-  while (n > 1) {
-    // Take the two smallest ropes
-    const smallestRope = ropeLengths.shift();
-    const secondSmallestRope = ropeLengths.shift();
+		
 
-    // Connect the two smallest ropes and add the cost to the total
-    const connectedRope = smallestRope + secondSmallestRope;
-    totalCost += connectedRope;
-
-    // Add the connected rope back to the list of ropes in the correct position
-    let i = 0;
-    while (i < n - 1 && ropeLengths[i] < connectedRope) {
-      i++;
-    }
-    ropeLengths.splice(i, 0, connectedRope);
-
-    // Decrement n
-    n--;
-  }
-
-  // Return the total cost
-  return totalCost;
+		arr.sort(function (a,b) {return a-b})
+	}
+  //your code here
+  document.getElementById("result").innerHTML = cost
+  
 }
